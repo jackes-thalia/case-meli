15ecac353714c311a14dda07946039443dcf860c
Thalia Jackes <jackesthalia7@gmail.com>
2024-02-09 23:41:29 -0300

    feat: add steps to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,14 @@
 'use client'
+import React from 'react'
 import MainTemplate from '@/presentation/container/MainTemplate'
 import Card from '@/presentation/components/Card/Card'
 import { Button, TextField } from '@mui/material'
+import { redirect } from 'next/navigation'
 
 export const Home = () => {
-  return (
-    <MainTemplate>
+  const [step, setStep] = React.useState<number>(1)
+  const emailSection = () => {
+    return (
       <Card>
         <h2>Olá! Informe seu e-mail Meli para solicitar uma ocorrência</h2>
         <label>Digite o email:</label>
@@ -15,12 +18,55 @@ export const Home = () => {
           type="email"
           variant="outlined"
         />
-        <Button sx={{ mt: 5 }} size="large" variant="contained">
+        <Button
+          sx={{ mt: 5 }}
+          size="large"
+          variant="contained"
+          onClick={nextStep}
+        >
           Continuar
         </Button>
       </Card>
-    </MainTemplate>
-  )
+    )
+  }
+  const tokenSection = () => {
+    return (
+      <Card>
+        <h2>Agora, Informe o token que enviamos para o seu e-mail</h2>
+        <label>Digite o token:</label>
+        <TextField
+          id="input-email"
+          name="email"
+          type="email"
+          variant="outlined"
+        />
+        <Button
+          sx={{ mt: 5 }}
+          size="large"
+          variant="contained"
+          onClick={nextStep}
+        >
+          Continuar
+        </Button>
+      </Card>
+    )
+  }
+  const renderStep = () => {
+    const steps: { [key: number]: () => React.JSX.Element } = {
+      1: emailSection,
+      2: tokenSection,
+    }
+    return steps[step]() || null
+  }
+  const nextStep = () => {
+    if (step === 2) {
+      // TODO: save user and redirect to dashboard
+      console.log('redirect')
+      redirect('/dashboard')
+    }
+    setStep((s) => s + 1)
+  }
+  return <MainTemplate>{renderStep()}</MainTemplate>
 }
 
 export default Home
