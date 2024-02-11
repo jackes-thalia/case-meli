b275e23664e06e208372e99ba3363f001f0d2be1
Thalia Jackes <jackesthalia7@gmail.com>
2024-02-11 20:04:07 -0300

    style: remove style

--- src/presentation/components/Card/Card.styles.ts
+++ src/presentation/components/Card/Card.styles.ts
@@ -14,20 +14,6 @@ export const StyledCard = styled.div<StyledCardProps>`
   display: flex;
   flex-direction: column;
 
-  h2 {
-    font-weight: 600;
-    font-size: 24px;
-    line-height: 26px;
-    margin-bottom: 16px;
-  }
-
-  label {
-    font-weight: 300;
-    font-size: 14px;
-    line-height: 20px;
-    margin: 12px 0 8px;
-  }
-
   @media (min-width: ${breakpoints.small}) {
     padding: 94px 92px;
     width: ${({ size }) =>
