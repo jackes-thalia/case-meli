3b08f9a7624ae184710e762fdb6a4e7c1688857c
Thalia Jackes <jackesthalia7@gmail.com>
2024-02-10 21:27:00 -0300

    feat: create upload component

--- src/app/dashboard/components/ThirdStep/ThirdStep.styles.ts
+++ src/app/dashboard/components/ThirdStep/ThirdStep.styles.ts
@@ -1,12 +1,50 @@
+import breakpoints from '@/presentation/constansts/breakpoints'
 import styled from 'styled-components'
 
-export const StyledThirdStep = styled.div`
+export const StyledThirdStep = styled.form`
   display: flex;
   flex-direction: column;
   padding-top: 60px;
 
   .upload-content {
     display: grid;
-    grid-template-columns: 1fr 220px;
+    gap: 20px;
+    grid-template-columns: 1fr;
+  }
+
+  .files-list {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 8px 16px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.12);
+    border-radius: 8px;
+
+    & > li {
+      display: flex;
+      justify-content: space-between;
+
+      & > span {
+        white-space: nowrap;
+        max-width: 200px;
+        text-overflow: ellipsis;
+        overflow: hidden;
+      }
+    }
+  }
+
+  @media (min-width: ${breakpoints.small}) {
+    .upload-content {
+      grid-template-columns: 1fr 220px;
+    }
+    .files-list {
+      & > li {
+        & > span {
+          max-width: 300px;
+        }
+      }
+    }
   }
 `
--- src/app/dashboard/components/ThirdStep/ThirdStep.tsx
+++ src/app/dashboard/components/ThirdStep/ThirdStep.tsx
@@ -1,11 +1,50 @@
 'use client'
 
 import React from 'react'
-import { Button, TextField, Typography } from '@mui/material'
+import { Button, IconButton, TextField, Typography } from '@mui/material'
 import { StyledThirdStep } from './ThirdStep.styles'
 import LocationInfo from '../LocationInfo/LocationInfo'
+import CloseIcon from '@mui/icons-material/Close'
+import { StepContext } from '@/presentation/providers/StepProvider'
 
 const ThirdStep = () => {
+  const [filesToUpload, setFilesToUpload] = React.useState<File[]>([])
+  const { nextStep } = React.useContext(StepContext)
+
+  const addFiles = (files: File[]) => {
+    setFilesToUpload((currentFiles) => {
+      return [...currentFiles, ...files]
+    })
+  }
+  const onFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target
+    if (!files) {
+      return
+    }
+    const sumUploads = filesToUpload.length + files.length
+    if (sumUploads > 3) {
+      alert('Você só pode enviar no máximo 3 arquivos')
+      return
+    }
+    console.log(files)
+    let valid = true
+    Array.from(files).forEach((file) => {
+      if (file.size > 30 * 1000 * 1000) {
+        valid = false
+      }
+    })
+    if (!valid) {
+      event.target.value = ''
+      alert('O arquivo não pode ser maior que 30mb')
+      return
+    }
+    addFiles(Array.from(files))
+  }
+  const removeFile = (keyToRemove: number) => {
+    setFilesToUpload((currentFiles) => {
+      return currentFiles.filter((_, key) => key !== keyToRemove)
+    })
+  }
   return (
     <StyledThirdStep>
       <LocationInfo />
@@ -17,23 +56,61 @@ const ThirdStep = () => {
           <Typography>
             Anexe arquivos que facilitem a análise da ocorrência
           </Typography>
+          <Button variant="contained" component="label">
+            Adicionar arquivo
+            <input
+              onChange={onFileUpload}
+              name="files[]"
+              hidden
+              type="file"
+              multiple
+            />
+          </Button>
+          {filesToUpload.length > 0 && (
+            <ul className="files-list">
+              {filesToUpload.map((file, key) => (
+                <li key={file.name}>
+                  <span title={file.name}>{file.name}</span>
+                  <IconButton
+                    sx={{
+                      px: 0,
+                      py: 0,
+                    }}
+                    size="small"
+                    aria-label="delete"
+                    onClick={() => removeFile(key)}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                </li>
+              ))}
+            </ul>
+          )}
         </section>
 
         <section>
           <Typography>Observações</Typography>
           <TextField
-            id="input-comments"
             name="comments"
+            placeholder="Digite uma observação"
             type="text"
             variant="outlined"
             size="medium"
             fullWidth
+            multiline
+            inputProps={{ maxLength: 140 }}
+            rows={7}
             sx={{ mt: 3 }}
           />
         </section>
       </div>
 
-      <Button variant="contained" size="large" sx={{ mt: 6 }}>
+      <Button
+        variant="contained"
+        size="large"
+        sx={{ mt: 6 }}
+        onClick={nextStep}
+      >
         Avançar
       </Button>
     </StyledThirdStep>
