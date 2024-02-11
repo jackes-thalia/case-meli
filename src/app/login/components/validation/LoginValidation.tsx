e4788d8e3e331ef7956bcdb85e6340b923799dd6
Thalia Jackes <jackesthalia7@gmail.com>
2024-02-11 12:41:48 -0300

    feat: create loginValidation schema

---
+++ src/app/login/components/validation/LoginValidation.tsx
@@ -0,0 +1,13 @@
+import * as Yup from 'yup'
+
+export const emailValidationSchema = Yup.object({
+  email: Yup.string()
+    .email('E-mail inválido')
+    .required('O e-mail é obrigatório'),
+}).required()
+
+export const tokenValidationSchema = Yup.object({
+  token: Yup.string()
+    .matches(/^\d{6}$/, 'O campo deve ter 6 dígitos')
+    .required('O e-mail é obrigatório'),
+}).required()
